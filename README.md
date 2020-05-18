cf3d2146512d24f9c36d2f2c05809e025eddf6cf
Karan Suraj <karan.suraj@outco.co>
2020-05-18 13:51:21 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+*General Strategy
+1. Work on calling google translate from flask REST endpoints
+2. Deploy flask server to netlify or other location via github CI/CD
+3. Work on calling Telegram API from flask server
+4. Work on creating Telegram responses
+
+
+
+Install Google Cloud SDK on server and include JSON Authentication Private Key as Global Var
+export GOOGLE_APPLICATION_CREDENTIALS="[PATH]"
+
+
+
+https://cloud.google.com/translate/docs/basic/setup-basic
+
+
+export GOOGLE_APPLICATION_CREDENTIALS="/Users/karansuraj/Repos/TeleTransBot/TeleTransBot-39a9b61fdafa.json"
---
+++ gtrans.py
@@ -0,0 +1,53 @@
+import flask
+import requests
+from flask import request, jsonify
+from translate import detect_language, translate_text
+
+import sqlite3
+
+
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+# app.config['DATABASE'] = 'school.db'
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """ Used to handle 404 errors """
+    print(e)
+    return "<h1>404</h1><p>The resource could not be found.</p>", 404
+
+
+@app.route('/', methods=['GET'])
+def home():
+    """ Home page """
+    return '''<h1>Google Translate Bot</h1>
+<p>An API translating non-english sentences to English ones for a Telegram bot.</p>'''
+
+@app.route('/detect', methods=['GET'])
+def detectLanguage():
+    """ REST API call to detect language of text """
+    query_parameters = request.args
+    text = query_parameters.get('text')
+    if not text:
+        return ""
+    result = detect_language(text)
+    return result
+
+
+@app.route('/translate', methods=['GET'])
+def translate():
+    """ REST API call to translate whatever language text to English """
+    query_parameters = request.args
+    text = query_parameters.get('text')
+    if not text:
+        return ''
+    translated = translate_text('en',text)
+    if not translated:
+        return ''
+    return translated
+
+
+if __name__ == '__main__':
+    app.run()
